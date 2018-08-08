41366d7113822ec3614c2b6e595e4d3ad410d989
annie8218 <annguyen8218@gmail.com>
2018-08-08 09:27:53 -0700

    Add readData()

--- src/library/ReadLibraryApplication.java
+++ src/library/ReadLibraryApplication.java
@@ -3,6 +3,7 @@ package library;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 import java.io.BufferedReader;
 
 
@@ -10,10 +11,26 @@ public class ReadLibraryApplication {
 	public static void main(String args[]) {
 		StringBuffer books = readFile("/Users/annie/Desktop/java/Books.txt");
 		StringBuffer students = readFile("/Users/annie/Desktop/java/Students.txt");
-		System.out.println(books);
-		System.out.println(students);
+		
+		String bookLine[] = books.toString().split("\n");
+		String bookID[] = readData(bookLine, 0);
+		String bookName[] = readData(bookLine, 1);
+		String borrowed[] = readData(bookLine, 6);
+		System.out.println(Arrays.toString(borrowed));
+		
+		// List names of books that are being borrowed
+		System.out.print("Books borrowed: ");
+		for (int i = 0; i < borrowed.length; i ++) {
+			if (borrowed[i].equalsIgnoreCase("yes")) {
+				System.out.print(bookName[i] + ", ");
+			} 
+		}
 		
 		
+		// List names of books that are not being borrowed.
+		
+				
+
 	}
 	
 	public static StringBuffer readFile(String FilePath) {
@@ -34,4 +51,13 @@ public class ReadLibraryApplication {
 		return stringBuffer;
 	}
 	
+	public static String[] readData(String[] line, int index) {
+		String[] data =  new String[line.length - 1];
+		for (int i = 1; i < line.length; i ++) {
+//			System.out.println(line[i]);
+			data[i - 1] = line[i].split(", ")[index];
+//			System.out.println(data[i - 1]);
+		}
+		return data;
+	}
 }
